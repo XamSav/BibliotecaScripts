1100c3d2c47645686d95982ba3339aa2962a032f
Kiznaiver27 <60893142+Kiznaiver27@users.noreply.github.com>
2022-08-05 23:25:35 +0200

    GET y JSON
    Peticion GET y conversion de JSON a Array de un Objeto

---
+++ Ranking_HTTP.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using Newtonsoft.Json;
+public class Ranking_HTTP : MonoBehaviour
+{
+    #region Buttons
+    public void GoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Reload()
+    {
+        StartCoroutine(GetRanking());
+    }
+    #endregion
+
+    public string url = "127.0.0.1";
+    void Start()
+    {
+        StartCoroutine(GetRanking());
+    }
+    IEnumerator GetRanking()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+            string[] pages = url.Split('/');
+            int page = pages.Length - 1;
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    string json = fixJson(webRequest.downloadHandler.text);
+                    Player[] a = JsonHelper.FromJson<Player>(json);
+                    Debug.Log(a[0]);
+                    break;
+            }
+        }
+    }
+    string fixJson(string value)
+    {
+        value = "{\"Items\":" + value + "}";
+        return value;
+    }
+       
+
+    [System.Serializable]
+    public class Player
+    {
+        public string name;
+        public int score;
+        public int tickets;
+        public int coockies;
+    }
+    #region JSON
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [System.Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    }
+    #endregion
+}
