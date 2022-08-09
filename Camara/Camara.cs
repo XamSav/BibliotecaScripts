87c86727b1a58765e103f9e58aae0fe0807e7850
Kiznaiver27 <60893142+Kiznaiver27@users.noreply.github.com>
2022-08-09 16:56:41 +0200

    Add files via upload

---
+++ Camara/Camara.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    //Para juegos 2d donde hay limites para seguir al jugador
+    private GameObject player;
+    [SerializeField]
+    private bool bounds = true;
+
+    [SerializeField]
+    private Vector3 minCameraPos;
+    [SerializeField]
+    private Vector3 MaxCameraPos;
+
+    private void OnEnable()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        
+       transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, MaxCameraPos.x), Mathf.Clamp(transform.position.y, minCameraPos.y, MaxCameraPos.y), Mathf.Clamp(transform.position.z, minCameraPos.z, MaxCameraPos.z));
+        }
+    }
+    
+}
